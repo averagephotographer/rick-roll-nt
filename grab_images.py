5fa8a3e6ee8c3598766e11c12b7799976b298f0b
averagephotographer <christopher.tullier98@gmail.com>
2022-04-15 01:09:54 -0500

    comments and ABBA

--- grab_images.py
+++ grab_images.py
@@ -3,6 +3,9 @@ import cv2
 from mss import mss
 from PIL import Image
 
+# this just works for windows screens
+# todo: a version for macos
+#! need to grant permission to VS code in settings to allow screen recording
 bouding_box = {'top': 38, 'left': 9, 'width': 1920, 'height': 1080}
 
 sct = mss()
